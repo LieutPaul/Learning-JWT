87b4bd2f73e08965f5f79e8ca4b52b8072dac0a6
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-01-26 19:54:16 +0530

    Refresh Token Implementation

--- authServer.js
+++ authServer.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.json());
 
+let refreshTokens = []
+
 function generateAccessToken(user){
     return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn : '15s'});
 }
@@ -15,11 +17,28 @@ function generateRefreshToken(user){
     return jwt.sign(user,process.env.REFRESH_TOKEN_SECRET);
 }
 
+app.post("/token", (req,res)=>{
+    const refreshToken = req.body.token;
+    if(refreshToken == null){
+        return res.status(401);
+    }
+    if(!refreshTokens.includes(refreshToken)){
+        return res.status(403);
+    }
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.status(403);
+        const accessToken = generateAccessToken({name:user.name});
+        res.json({accessToken : accessToken});
+    })
+});
+
+
 app.post("/login", (req,res)=>{
     const username = req.body.username
     const user = {name : username};
     const accessToken = generateAccessToken(user) //Creating token for the user
     const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
     res.json({accessToken : accessToken, refreshToken : refreshToken});
 });
 
